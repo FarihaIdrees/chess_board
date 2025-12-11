a33fa7bb88af7213e2bc0060ff1e5ecfae266c30
Fariha Idrees <fareihaidrees@gmail.com>
2025-12-11 13:44:43 -0800

    created basic architecture

--- lib/dashboard_view.dart
+++ lib/dashboard_view.dart
@@ -2,131 +2,173 @@ import 'package:flutter/material.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-        appBar: AppBar(title:Center(child: Text("CHESS BOARD",
-        style: TextStyle(color: Colors.white,fontSize: 20,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold ),),),
-        backgroundColor: Colors.black),
-        body: Center(
-          child: Column(
-            children: [
-              // Row 1
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                ],
-              ),
+      appBar: AppBar(title: Center(child: Text("CHESS BOARD",style: TextStyle
+      (color: Colors.white,fontSize: 22),),),
+      backgroundColor: Colors.black,),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Row 1
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [ Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+               
+              ],
+            ),
 
-              // Row 2
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                ],
-              ),
+            // Row 2
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                
+              ],
+            ),
 
-              // Row 3
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                ],
-              ),
+            // Row 3
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [ Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                
+              ],
+            ),
 
-              // Row 4
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                ],
-              ),
+            // Row 4
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+              ],
+            ),
 
-              // Row 5
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                ],
-              ),
+            // Row 5
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+              ],
+            ),
 
-              // Row 6
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                ],
-              ),
+            // Row 6
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+               Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+              ],
+            ),
 
-              // Row 7
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                ],
-              ),
+            // Row 7
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+                Container(width: 60, height: 60, color: Colors.white),
+                Container(width: 60, height: 60, color: Colors.black),
+              ],
+            ),
 
-              // Row 8
-              Row(
-                children: [
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                  Container(width: 60, height: 60, color: Colors.white),
-                  Container(width: 60, height: 60, color: Colors.black),
-                ],
-              ),
-            ],
-          ),
+            // Row 8
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+               Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width:60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.black,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+                Container(width: 60, height: 60, color: Colors.white,child: 
+              DecoratedBox(decoration:BoxDecoration
+              (border: BoxBorder.all(color: Colors.black,width: 1))),),
+              ],
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
